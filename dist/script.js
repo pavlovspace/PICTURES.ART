def156f6af6fa52cf4a9e3a75e6b58b257fc7f80
Paul Pavlov <pavlov1space@gmail.com>
2023-12-02 12:31:05 +0100

    implemented the modals module

--- dist/script.js
+++ dist/script.js
@@ -13,28 +13,84 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay) {
     const triggers = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector),
-      windows = document.querySelectorAll('[data-modal]');
-    triggers.forEach(element => {
-      element.addEventListener('click', () => {
-        modal.style.display = 'flex';
-      });
-    });
-    close.addEventListener('click', () => {
-      modal.style.display = 'none';
-    });
-    windows.forEach(element => {
-      element.addEventListener('click', event => {
-        if (event.target == element) {
-          modal.style.display = 'none';
+      windows = document.querySelectorAll('[data-modal]'),
+      scroll = calcScroll();
+
+    // Opening Modal on Trigger Click
+    triggers.forEach(item => {
+      item.addEventListener('click', e => {
+        if (e.target) {
+          e.preventDefault();
         }
+        windows.forEach(item => {
+          item.style.display = 'none';
+        });
+        modal.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
+
+    // Closing Modal on Close Button Click
+    close.addEventListener('click', () => {
+      windows.forEach(element => {
+        element.style.display = 'none';
+      });
+      modal.style.display = 'none';
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
+    });
+
+    // Closing Modal on Overlay Click
+    modal.addEventListener('click', e => {
+      if (e.target === modal && closeClickOverlay === true) {
+        windows.forEach(element => {
+          element.style.display = 'none';
+        });
+        modal.style.display = 'none';
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`;
+      }
+    });
+
+    // Function to calculate browser scrollbar width
+    function calcScroll() {
+      let div = document.createElement('div');
+      div.style.width = '50px';
+      div.style.height = '50px';
+      div.style.overflowY = 'scroll';
+      div.style.visibility = 'hidden';
+      document.body.appendChild(div);
+      let scrollWidth = div.offsetWidth - div.clientWidth;
+      div.remove();
+      return scrollWidth;
+    }
+
+    // Function to display a modal window after a certain time
+    function showModalByTime(selector, time) {
+      setTimeout(() => {
+        let display = false;
+        document.querySelectorAll('[data-modal]').forEach(element => {
+          if (getComputedStyle(element).display !== 'none') {
+            display = 'block';
+          }
+        });
+        if (display === false) {
+          let selectorPopup = document.querySelector(selector);
+          selectorPopup.style.display = 'block';
+          document.body.style.overflow = 'hidden';
+        }
+      }, time);
+    }
+    // showModalByTime('.popup-consultation', 60000)
   }
-  bindModal('.button', '.popup-consultation', '.popup-content .popup-close');
+
+  bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close', true);
+  bindModal('.button-design', '.popup-design', '.popup-design .popup-close', true);
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (modals);
 
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,31 +1,91 @@
 const modals = () => {
-    function bindModal(triggerSelector, modalSelector, closeSelector) {
+    function bindModal(triggerSelector, modalSelector, closeSelector, closeClickOverlay) {
         const triggers = document.querySelectorAll(triggerSelector),
             modal = document.querySelector(modalSelector),
             close = document.querySelector(closeSelector),
-            windows = document.querySelectorAll('[data-modal]')
+            windows = document.querySelectorAll('[data-modal]'),
+            scroll = calcScroll()
 
-        triggers.forEach((element) => {
-            element.addEventListener('click', () => {
-                modal.style.display = 'flex'
-            })
-        })
-
-        close.addEventListener('click', () => {
-            modal.style.display = 'none'
-        })
-
-        windows.forEach((element) => {
-            element.addEventListener('click', (event) => {
-                
-                if (event.target == element) {
-                    modal.style.display = 'none'
+        // Opening Modal on Trigger Click
+        triggers.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault()
                 }
+
+                windows.forEach((item) => {
+                    item.style.display = 'none'
+                })
+
+                modal.style.display = 'block'
+                document.body.style.overflow = 'hidden'
+                document.body.style.marginRight = `${scroll}px`
             })
         })
+
+        // Closing Modal on Close Button Click
+        close.addEventListener('click', () => {
+            windows.forEach((element) => {
+                element.style.display = 'none'
+            })
+
+            modal.style.display = 'none'
+            document.body.style.overflow = ''
+            document.body.style.marginRight = `0px`
+        })
+
+        // Closing Modal on Overlay Click
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal && closeClickOverlay === true) {
+                windows.forEach((element) => {
+                    element.style.display = 'none'
+                })
+
+                modal.style.display = 'none'
+                document.body.style.overflow = ''
+                document.body.style.marginRight = `0px`
+            }
+        })
+
+        // Function to calculate browser scrollbar width
+        function calcScroll() {
+            let div = document.createElement('div')
+
+            div.style.width = '50px'
+            div.style.height = '50px'
+            div.style.overflowY = 'scroll'
+            div.style.visibility = 'hidden'
+
+            document.body.appendChild(div)
+            let scrollWidth = div.offsetWidth - div.clientWidth
+            div.remove()
+
+            return scrollWidth
+        }
+
+        // Function to display a modal window after a certain time
+        function showModalByTime(selector, time) {
+            setTimeout(() => {
+                let display = false
+
+                document.querySelectorAll('[data-modal]').forEach((element) => {
+                    if (getComputedStyle(element).display !== 'none') {
+                        display = 'block'
+                    }
+                })
+
+                if (display === false) {
+                    let selectorPopup = document.querySelector(selector)
+                    selectorPopup.style.display = 'block'
+                    document.body.style.overflow = 'hidden'
+                }
+            }, time)
+        }
+        // showModalByTime('.popup-consultation', 60000)
     }
 
-    bindModal('.button', '.popup-consultation', '.popup-content .popup-close')
+    bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close', true)
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close', true)
 }
 
 export default modals
